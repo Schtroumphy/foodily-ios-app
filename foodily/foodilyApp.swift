5101aad2175cb3a70ee9b9184408aa6f3473500d
tjeanlouis <tjeanlouis@ippon.fr>
2023-06-28 09:48:05 +0200

    Initial commit

---
+++ foodily/foodilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodilyApp.swift
+//  foodily
+//
+//  Created by JEAN-LOUIS Thessalène on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct foodilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
